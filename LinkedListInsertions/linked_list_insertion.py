97abe907df0806846b42745cca5d11325f60acef
Hamad Dwairi <hamaddwairi0@gmail.com>
2022-06-14 00:04:41 +0300

    Append , InsertAfter , insertBefore added using Py

---
+++ LinkedListInsertions/linked_list_insertion.py
@@ -0,0 +1,123 @@
+class LinkedList:
+
+    def __init__(self, head=None):
+        """
+        This is the constructor for the actual linked list.
+
+        Arguments: head (optional param that defaults to none and represents the head of the linked list).
+        """
+        self.head = head
+
+    def insert(self, value):
+        """
+        Insert a value to the head of the linked list.
+
+        Arguments: value (value that the new node represents)
+        """
+        node = Node(value)
+
+        if self.head is not None:
+            node.next = self.head
+
+        self.head = node
+
+    def includes(self, value):
+        """
+        Search the linked list for a specific value.
+
+        Arguments: value (value we are searching for in the list).
+
+        Output: A boolean (true of false).
+        """
+
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+
+            current = current.next
+
+        return False
+
+    def append(self, value):
+        """
+        Appends value to end of linked list
+        Arguments: value (value we trying to append).
+        """
+        node = Node(value)
+        current = self.head
+        if self.head == None:
+            self.head = node
+            return
+
+        while current.next is not None:
+            current = current.next
+
+        current.next = node
+
+    def insertBefore(self, value, newVal):
+        """
+        Inserts value before a specified value
+        Arguments: value and newVal (we are trying to insert newVal before value (if exists)).
+        """
+        current = self.head
+
+        if current.value == value:
+            self.insert(newVal)
+            return
+
+        while current is not None:
+            if current.next.value == value:
+                node = Node(newVal)
+                node.next = current.next
+                current.next = node
+                return
+            current = current.next
+
+        raise Exception(f"{{{value}}} does not exist in the linked list!")
+
+    def insertAfter(self, value, newVal):
+        """
+        Inserts value after a specified value
+        Arguments: value and newVal (we are trying to insert newVal after value (if exists)).
+        """
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                node = Node(newVal)
+                node.next = current.next
+                current.next = node
+                return
+            current = current.next
+
+        raise Exception(f"{{{value}}} does not exist in the linked list!")
+
+    def __str__(self):
+        """
+        Produce a string representation of the linked list.
+
+        Output: String representation of the linked list.
+        """
+
+        current = self.head
+        string = ''
+
+        while current is not None:
+            string += f"{{{current.value}}}->"
+            current = current.next
+
+        return string + 'NULL'
+
+
+class Node:
+
+    def __init__(self, value, next=None):
+        """
+        This is the Node Constructor
+
+        Arguments: value (value that the node represents) and next (optional param that defaults to none and represents the next node in the list).
+        """
+        self.value = value
+        self.next = next
---
+++ LinkedListInsertions/test_linked_list_insertion.py
@@ -0,0 +1,134 @@
+import pytest
+from linked_list_insertion import LinkedList, Node
+
+
+def test_import():
+    assert LinkedList
+
+# Test empty list
+def test_empty_list():
+    actual = LinkedList().head
+    expected = None
+    assert actual == expected
+
+# Test inserting node into list
+def test_insert_in_list():
+    link_list = LinkedList()
+    link_list.insert(75)
+    actual = link_list.head.value
+    expected = 75
+    assert actual == expected
+
+# Testing True for includes
+def test_list_includes_one():
+    node = Node(100)
+    actual = LinkedList(node).includes(100)
+    expected = True
+    assert actual == expected
+
+# Testing head points (->) to first node
+def test_list_head():
+    node = Node(45)
+    actual = LinkedList(node).head
+    expected = node
+    assert actual == expected
+
+# Testing inserting multiple nodes
+def test_insert_multiple_nodes():
+    val1 = 'Trump'
+    val2 = 'Melania'
+    val3 = 'Conway'
+    val4 = 'Huckabee'
+    linked_list = LinkedList(val1)
+    linked_list.insert(val2)
+    linked_list.insert(val3)
+    linked_list.insert(val4)
+    actual = linked_list.head.value
+    expected = 'Huckabee'
+    assert actual == expected
+
+# Testing False for includes
+def test_if_list_includes_two():
+    node = Node(50)
+    actual = LinkedList(node).includes(49)
+    expected = False
+    assert actual == expected
+
+# Testing the string method
+def test_linked_list_str():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('b')
+    link_list.insert('a')
+    actual = str(link_list)
+    expected = "{a}->{b}->{c}->NULL"
+    assert actual == expected
+
+# Add node to end of list
+def test_append_node_to_list():
+    link_list = LinkedList()
+    link_list.insert('b')
+    link_list.insert('c')
+    link_list.append('a')
+    actual = str(link_list)
+    expected = "{c}->{b}->{a}->NULL"
+    assert actual == expected
+
+# Add multiple nodes to end of list
+def test_append_nodes_to_list():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('d')
+    link_list.append('b')
+    link_list.append('a')
+    actual = str(link_list)
+    expected = "{d}->{c}->{b}->{a}->NULL"
+    assert actual == expected
+
+# insert before middle
+def test_insert_before_middle():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('d')
+    link_list.append('b')
+    link_list.append('a')
+    link_list.insertBefore('b', 'yasss')
+    actual = str(link_list)
+    expected = "{d}->{c}->{yasss}->{b}->{a}->NULL"
+    assert actual == expected
+
+# insert before first
+def test_insert_before_first():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('d')
+    link_list.append('b')
+    link_list.append('a')
+    link_list.insertBefore('d', 'yasss')
+    actual = str(link_list)
+    expected = "{yasss}->{d}->{c}->{b}->{a}->NULL"
+    assert actual == expected
+
+# insert after middle
+def test_insert_after_middle():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('d')
+    link_list.append('b')
+    link_list.append('a')
+    link_list.insertAfter('c', 'yasss')
+    actual = str(link_list)
+    expected = "{d}->{c}->{yasss}->{b}->{a}->NULL"
+    assert actual == expected
+
+# insert after last
+def test_insert_after_last():
+    link_list = LinkedList()
+    link_list.insert('c')
+    link_list.insert('d')
+    link_list.append('b')
+    link_list.append('a')
+    link_list.insertAfter('a', 'yasss')
+    actual = str(link_list)
+    expected = "{d}->{c}->{b}->{a}->{yasss}->NULL"
+    assert actual == expected
